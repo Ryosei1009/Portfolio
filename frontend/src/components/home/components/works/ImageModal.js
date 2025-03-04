9b33e571ab4d657b1c776eb3a34f5bf58d7c409a
Ryosei1009 <132128585+ShinoChan0@users.noreply.github.com>
2025-03-04 09:35:05 +0900

    Update ImageModal.js

--- frontend/src/components/home/components/works/ImageModal.js
+++ frontend/src/components/home/components/works/ImageModal.js
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react'
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 
 const ImageModal = ({ isOpen, setIsOpen, selectedImage, selectedWork }) => {
     return (
@@ -29,7 +29,7 @@ const ImageModal = ({ isOpen, setIsOpen, selectedImage, selectedWork }) => {
                         <Dialog.Panel className="relative max-w-5xl mx-auto">
                             <img
                                 className="w-full h-auto max-h-screen bg-white object-contain"
-                                src={`${process.env.REACT_APP_IMAGE_DOMAIN}/images/works/${selectedWork.id}/${selectedImage}.png}`}
+                                src={`${process.env.REACT_APP_IMAGE_DOMAIN}/images/works/${selectedWork.id}/${selectedImage}.png`}
                                 alt=""
                             />
                         </Dialog.Panel>
